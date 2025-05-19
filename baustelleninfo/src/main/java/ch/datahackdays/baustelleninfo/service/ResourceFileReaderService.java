ac670d676f957e8e06baa5a278439a6a5e513605
Samuel Wyss <samuel.wyss@bedag.ch>
2025-05-19 07:23:29 +0200

    [NO_ISSUE]: Fix file reading

--- baustelleninfo/src/main/java/ch/datahackdays/baustelleninfo/service/ResourceFileReaderService.java
+++ baustelleninfo/src/main/java/ch/datahackdays/baustelleninfo/service/ResourceFileReaderService.java
@@ -1,10 +1,14 @@
 package ch.datahackdays.baustelleninfo.service;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Collectors;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.lang.Nullable;
@@ -15,11 +19,13 @@ public class ResourceFileReaderService {
 
     public String readFileFromResources(String fileName) {
         try {
-            // Load the file from resources/data
-            Path path = new ClassPathResource("data/" + fileName).getFile().toPath();
+            // Load the file as an InputStream
+            InputStream inputStream = new ClassPathResource("data/" + fileName).getInputStream();
 
-            // Read the file content as a String
-            return Files.readString(path, StandardCharsets.UTF_8);
+            // Read the InputStream content as a String
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                return reader.lines().collect(Collectors.joining(System.lineSeparator()));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
